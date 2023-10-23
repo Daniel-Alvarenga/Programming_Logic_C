f6c71241211ac910826006591901490510604440
Daniel R. Alvarenga <128755697+Daniel-Alvarenga@users.noreply.github.com>
2023-10-23 11:55:32 -0300

    Update Cauculadora de notas.cpp

--- 04.2022/04.04.22/Cauculadora de Aprovação/Cauculadora de notas.cpp
+++ 04.2022/04.04.22/Cauculadora de Aprovação/Cauculadora de notas.cpp
@@ -1,4 +1,4 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h> 
-int main () {setlocale(LC_ALL,"Portuguese");float A, B, C, med;	printf("Olá querido aluno\n\n\n");printf("Insira suas notas e vereifique seu estado quanto a ser aprovado ou reprovado:\n");printf ("Nota 1:");scanf("%f", &A);printf ("Nota 2:");scanf("%f", &B);printf ("Nota 3:");scanf("%f", &C);med=(A+B+C)/3;if(med>=6){printf("\nNão fez mais que a obrigacação (APROVADO)Sua Média foi :%.2f \n\n\n\n\n\n\n\n",med);}else {printf("SEM CULULAR UM MÊS (REPROVOU) Sua Média foi : %.2f \n\n\n\n\n\n\n\n",med);}system("pause");}
+int main () {setlocale(LC_ALL,"Portuguese");float A, B, C, med;	printf("OlÃ¡ querido aluno\n\n\n");printf("Insira suas notas e vereifique seu estado quanto a ser aprovado ou reprovado:\n");printf ("Nota 1:");scanf("%f", &A);printf ("Nota 2:");scanf("%f", &B);printf ("Nota 3:");scanf("%f", &C);med=(A+B+C)/3;if(med>=6){printf("\nNÃ£o fez mais que a obrigacaÃ§Ã£o (APROVADO)Sua MÃ©dia foi :%.2f \n\n\n\n\n\n\n\n",med);}else {printf("SEM CELULAR UM MÃŠS (REPROVOU) Sua MÃ©dia foi : %.2f \n\n\n\n\n\n\n\n",med);}system("pause");}
